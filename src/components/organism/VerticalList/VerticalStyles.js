e85ef6d01c86df8006ff5e4ec3129e7b9b3af4cf
Deniz-Navarro <ldeniz1@ucol.mx>
2022-06-09 18:07:00 -0500

    Create the screen My Flights.
    We created the screen my Flights and we did code 2 new components to show the flights.
    
    Co-authored-by: Gerardo Emmanuel Tovar Barragan <geemtovarba@ittepic.edu.mx>

---
+++ src/components/organism/VerticalList/VerticalStyles.js
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native';
+
+const VerticalStyles = StyleSheet.create({
+  container: {
+    margin: 15,
+    padding: 10,
+  },
+});
+
+export default VerticalStyles;
